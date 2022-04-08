43274f521153bb9d315c5c29dab5471422fe1cd5
gustirizkia <gustidoag936@gmail.com>
2022-04-09 02:28:22 +0700

    add register page

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -36,14 +36,34 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
         Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
 
-        RateLimiter::for('login', function (Request $request) {
-            $email = (string) $request->email;
+        // RateLimiter::for('login', function (Request $request) {
+        //     $email = (string) $request->email;
 
-            return Limit::perMinute(5)->by($email.$request->ip());
+        //     return Limit::perMinute(5)->by($email.$request->ip());
+        // });
+
+        // RateLimiter::for('two-factor', function (Request $request) {
+        //     return Limit::perMinute(5)->by($request->session()->get('login.id'));
+        // });
+
+        //register
+        Fortify::registerView(function () {
+            return view('pages.auth.register');
         });
 
-        RateLimiter::for('two-factor', function (Request $request) {
-            return Limit::perMinute(5)->by($request->session()->get('login.id'));
+        //login
+        Fortify::loginView(function () {
+            return view('pages.auth.login');
+        });
+
+        //forgot
+        Fortify::requestPasswordResetLinkView(function () {
+            return view('auth.forgot-password');
+        });
+
+        //reset
+        Fortify::resetPasswordView(function ($request) {
+            return view('auth.reset-password', ['request' => $request]);
         });
     }
 }
