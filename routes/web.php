cefa63d215a79da21e23f1324c724233e261ae40
gustiyokesen <gustimaulana.yokese@gmail.com>
2022-05-19 00:17:44 +0700

    add route home

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('home', function(){
+    return redirect('/admin');
+});
+
 Route::get('hello', function(){
     return view('pages.admin.dashboard');
 });
